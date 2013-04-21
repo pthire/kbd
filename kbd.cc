eee67f8d748acb62fa2eb5829668b5c8534464a4
pthire <patrice.thire@gmail.com>
2013-04-21 10:52:33 +0200

    ugly patch to make backward comptatible v0.8.x

--- kbd.cc
+++ kbd.cc
@@ -31,20 +31,20 @@ using namespace v8;
 // We use a struct to store information about the asynchronous "work request".
 
 struct Baton {
-    // This handle holds the callback function we'll call after the work request
-    // has been completed in a threadpool thread. It's persistent so that V8
-    // doesn't garbage collect it away while our request waits to be processed.
-    // This means that we'll have to dispose of it later ourselves.
-    Persistent<Function> callback;
+	// This handle holds the callback function we'll call after the work request
+	// has been completed in a threadpool thread. It's persistent so that V8
+	// doesn't garbage collect it away while our request waits to be processed.
+	// This means that we'll have to dispose of it later ourselves.
+	Persistent<Function> callback;
 
-    // Tracking errors that happened in the worker function. You can use any
-    // variables you want. E.g. in some cases, it might be useful to report
-    // an error number.
-    bool error;
-    std::string error_message;
+	// Tracking errors that happened in the worker function. You can use any
+	// variables you want. E.g. in some cases, it might be useful to report
+	// an error number.
+	bool error;
+	std::string error_message;
 
-    // Custom data you can pass through.
-    char chaine[1000];
+	// Custom data you can pass through.
+	char chaine[1000];
 };
 
 //===========================================================================
@@ -148,9 +148,9 @@ Handle<Value> GetKeySync(const Arguments& args) {
 // If you want to use parameters passed into the original call, you have to
 // convert them to PODs or some other fancy method.
 void getKeyAsyncWork(uv_work_t* req) {
-    Baton* baton = static_cast<Baton*>(req->data);
+	Baton* baton = static_cast<Baton*>(req->data);
 
-    // Do work in threadpool here.
+	// Do work in threadpool here.
 
 	struct termios t;
 	int len;
@@ -170,93 +170,98 @@ void getKeyAsyncWork(uv_work_t* req) {
 
 	t.c_lflag |= (ECHO | ICANON);
 
-    // If the work we do fails, set baton->error_message to the error string
-    // and baton->error to true.
+	// If the work we do fails, set baton->error_message to the error string
+	// and baton->error to true.
 }
 
 // This function is executed in the main V8/JavaScript thread. That means it's
 // safe to use V8 functions again. Don't forget the HandleScope!
+#if(UV_VERSION_MAJOR == 0 && UV_VERSION_MINOR == 8)
+void getKeyAsyncAfter(uv_work_t* req) {
+#else
 void getKeyAsyncAfter(uv_work_t* req, int x) {
-    HandleScope scope;
-    Baton* baton = static_cast<Baton*>(req->data);
+#endif
 
-    if (baton->error) {
-        Local<Value> err = Exception::Error(String::New(baton->error_message.c_str()));
+	HandleScope scope;
+	Baton* baton = static_cast<Baton*>(req->data);
 
-        // Prepare the parameters for the callback function.
-        const unsigned argc = 1;
-        Local<Value> argv[argc] = { err };
+	if (baton->error) {
+		Local<Value> err = Exception::Error(String::New(baton->error_message.c_str()));
 
-        // Wrap the callback function call in a TryCatch so that we can call
-        // node's FatalException afterwards. This makes it possible to catch
-        // the exception from JavaScript land using the
-        // process.on('uncaughtException') event.
-        TryCatch try_catch;
-        baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
-        if (try_catch.HasCaught()) {
-            node::FatalException(try_catch);
-        }
-    } else {
-        // In case the operation succeeded, convention is to pass null as the
-        // first argument before the result arguments.
-        // In case you produced more complex data, this is the place to convert
-        // your plain C++ data structures into JavaScript/V8 data structures.
-        const unsigned argc = 2;
-        Local<Value> argv[argc] = {
-            Local<Value>::New(Null()),
-            Local<Value>::New(String::New(baton->chaine))
-        };
+		// Prepare the parameters for the callback function.
+		const unsigned argc = 1;
+		Local<Value> argv[argc] = { err };
 
-        // Wrap the callback function call in a TryCatch so that we can call
-        // node's FatalException afterwards. This makes it possible to catch
-        // the exception from JavaScript land using the
-        // process.on('uncaughtException') event.
-        TryCatch try_catch;
-        baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
-        if (try_catch.HasCaught()) {
-            node::FatalException(try_catch);
-        }
-    }
+		// Wrap the callback function call in a TryCatch so that we can call
+		// node's FatalException afterwards. This makes it possible to catch
+		// the exception from JavaScript land using the
+		// process.on('uncaughtException') event.
+		TryCatch try_catch;
+		baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
+		if (try_catch.HasCaught()) {
+			node::FatalException(try_catch);
+		}
+	} else {
+		// In case the operation succeeded, convention is to pass null as the
+		// first argument before the result arguments.
+		// In case you produced more complex data, this is the place to convert
+		// your plain C++ data structures into JavaScript/V8 data structures.
+		const unsigned argc = 2;
+		Local<Value> argv[argc] = {
+			Local<Value>::New(Null()),
+			Local<Value>::New(String::New(baton->chaine))
+		};
 
-    // The callback is a permanent handle, so we have to dispose of it manually.
-    baton->callback.Dispose();
+		// Wrap the callback function call in a TryCatch so that we can call
+		// node's FatalException afterwards. This makes it possible to catch
+		// the exception from JavaScript land using the
+		// process.on('uncaughtException') event.
+		TryCatch try_catch;
+		baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
+		if (try_catch.HasCaught()) {
+			node::FatalException(try_catch);
+		}
+	}
 
-    // We also created the baton and the work_req struct with new, so we have to
-    // manually delete both.
-    delete baton;
-    delete req;
+	// The callback is a permanent handle, so we have to dispose of it manually.
+	baton->callback.Dispose();
+
+	// We also created the baton and the work_req struct with new, so we have to
+	// manually delete both.
+	delete baton;
+	delete req;
 }
 
 // This is the function called directly from JavaScript land. It creates a
 // work request object and schedules it for execution.
 Handle<Value> GetKey(const Arguments& args) {
-    HandleScope scope;
+	HandleScope scope;
 
-    if (!args[0]->IsFunction()) {
-        return ThrowException(Exception::TypeError(
-            String::New("First argument must be a callback function")));
-    }
-    // There's no ToFunction(), use a Cast instead.
-    Local<Function> callback = Local<Function>::Cast(args[0]);
+	if (!args[0]->IsFunction()) {
+		return ThrowException(Exception::TypeError(
+			String::New("First argument must be a callback function")));
+	}
+	// There's no ToFunction(), use a Cast instead.
+	Local<Function> callback = Local<Function>::Cast(args[0]);
 
-    // The baton holds our custom status information for this asynchronous call,
-    // like the callback function we want to call when returning to the main
-    // thread and the status information.
-    Baton* baton = new Baton();
-    baton->error = false;
-    baton->callback = Persistent<Function>::New(callback);
+	// The baton holds our custom status information for this asynchronous call,
+	// like the callback function we want to call when returning to the main
+	// thread and the status information.
+	Baton* baton = new Baton();
+	baton->error = false;
+	baton->callback = Persistent<Function>::New(callback);
 
-    // This creates the work request struct.
-    uv_work_t *req = new uv_work_t();
-    req->data = baton;
+	// This creates the work request struct.
+	uv_work_t *req = new uv_work_t();
+	req->data = baton;
 
-    // Schedule our work request with libuv. Here you can specify the functions
-    // that should be executed in the threadpool and back in the main thread
-    // after the threadpool function completed.
-    int status = uv_queue_work(uv_default_loop(), req, getKeyAsyncWork, getKeyAsyncAfter);
-    assert(status == 0);
+	// Schedule our work request with libuv. Here you can specify the functions
+	// that should be executed in the threadpool and back in the main thread
+	// after the threadpool function completed.
+	int status = uv_queue_work(uv_default_loop(), req, getKeyAsyncWork, getKeyAsyncAfter);
+	assert(status == 0);
 
-    return Undefined();
+	return Undefined();
 }
 //===========================================================================
 // GetLineSync
@@ -288,10 +293,10 @@ Handle<Value> GetLineSync(const Arguments& args) {
 // convert them to PODs or some other fancy method.
 
 void AsyncWork(uv_work_t* req) {
-    Baton* baton = static_cast<Baton*>(req->data);
+	Baton* baton = static_cast<Baton*>(req->data);
 	char* s;
 
-    // Do work in threadpool here.
+	// Do work in threadpool here.
 
 	s = fgets(baton->chaine, 1000, stdin);
 	if(s != NULL) {
@@ -300,96 +305,100 @@ void AsyncWork(uv_work_t* req) {
 		baton->chaine[0] = '\0';
 	}
 
-    // If the work we do fails, set baton->error_message to the error string
-    // and baton->error to true.
+	// If the work we do fails, set baton->error_message to the error string
+	// and baton->error to true.
 }
 
 // This function is executed in the main V8/JavaScript thread. That means it's
 // safe to use V8 functions again. Don't forget the HandleScope!
 
+#if(UV_VERSION_MAJOR == 0 && UV_VERSION_MINOR == 8)
+void AsyncAfter(uv_work_t* req) {
+#else
 void AsyncAfter(uv_work_t* req, int x) {
-    HandleScope scope;
-    Baton* baton = static_cast<Baton*>(req->data);
+#endif
+	HandleScope scope;
+	Baton* baton = static_cast<Baton*>(req->data);
 
-    if (baton->error) {
-        Local<Value> err = Exception::Error(String::New(baton->error_message.c_str()));
+	if (baton->error) {
+		Local<Value> err = Exception::Error(String::New(baton->error_message.c_str()));
 
-        // Prepare the parameters for the callback function.
-        const unsigned argc = 1;
-        Local<Value> argv[argc] = { err };
+		// Prepare the parameters for the callback function.
+		const unsigned argc = 1;
+		Local<Value> argv[argc] = { err };
 
-        // Wrap the callback function call in a TryCatch so that we can call
-        // node's FatalException afterwards. This makes it possible to catch
-        // the exception from JavaScript land using the
-        // process.on('uncaughtException') event.
-        TryCatch try_catch;
-        baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
-        if (try_catch.HasCaught()) {
-            node::FatalException(try_catch);
-        }
-    } else {
-        // In case the operation succeeded, convention is to pass null as the
-        // first argument before the result arguments.
-        // In case you produced more complex data, this is the place to convert
-        // your plain C++ data structures into JavaScript/V8 data structures.
-        const unsigned argc = 2;
-        Local<Value> argv[argc] = {
-            Local<Value>::New(Null()),
-            //Local<Value>::New(String::New(baton->chaine))
-            Local<Value>::New(String::New(baton->chaine))
-        };
+		// Wrap the callback function call in a TryCatch so that we can call
+		// node's FatalException afterwards. This makes it possible to catch
+		// the exception from JavaScript land using the
+		// process.on('uncaughtException') event.
+		TryCatch try_catch;
+		baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
+		if (try_catch.HasCaught()) {
+			node::FatalException(try_catch);
+		}
+	} else {
+		// In case the operation succeeded, convention is to pass null as the
+		// first argument before the result arguments.
+		// In case you produced more complex data, this is the place to convert
+		// your plain C++ data structures into JavaScript/V8 data structures.
+		const unsigned argc = 2;
+		Local<Value> argv[argc] = {
+			Local<Value>::New(Null()),
+			//Local<Value>::New(String::New(baton->chaine))
+			Local<Value>::New(String::New(baton->chaine))
+		};
 
-        // Wrap the callback function call in a TryCatch so that we can call
-        // node's FatalException afterwards. This makes it possible to catch
-        // the exception from JavaScript land using the
-        // process.on('uncaughtException') event.
-        TryCatch try_catch;
-        baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
-        if (try_catch.HasCaught()) {
-            node::FatalException(try_catch);
-        }
-    }
+		// Wrap the callback function call in a TryCatch so that we can call
+		// node's FatalException afterwards. This makes it possible to catch
+		// the exception from JavaScript land using the
+		// process.on('uncaughtException') event.
+		TryCatch try_catch;
+		baton->callback->Call(Context::GetCurrent()->Global(), argc, argv);
+		if (try_catch.HasCaught()) {
+			node::FatalException(try_catch);
+		}
+	}
 
-    // The callback is a permanent handle, so we have to dispose of it manually.
-    baton->callback.Dispose();
+	// The callback is a permanent handle, so we have to dispose of it manually.
+	baton->callback.Dispose();
 
-    // We also created the baton and the work_req struct with new, so we have to
-    // manually delete both.
-    delete baton;
-    delete req;
+	// We also created the baton and the work_req struct with new, so we have to
+	// manually delete both.
+	delete baton;
+	delete req;
 }
 
 // This is the function called directly from JavaScript land. It creates a
 // work request object and schedules it for execution.
 
 Handle<Value> GetLine(const Arguments& args) {
-    HandleScope scope;
+	HandleScope scope;
 
-    if (!args[0]->IsFunction()) {
-        return ThrowException(Exception::TypeError(
-            String::New("First argument must be a callback function")));
-    }
-    // There's no ToFunction(), use a Cast instead.
-    Local<Function> callback = Local<Function>::Cast(args[0]);
+	if (!args[0]->IsFunction()) {
+		return ThrowException(Exception::TypeError(
+			String::New("First argument must be a callback function")));
+	}
+	// There's no ToFunction(), use a Cast instead.
+	Local<Function> callback = Local<Function>::Cast(args[0]);
 
-    // The baton holds our custom status information for this asynchronous call,
-    // like the callback function we want to call when returning to the main
-    // thread and the status information.
-    Baton* baton = new Baton();
-    baton->error = false;
-    baton->callback = Persistent<Function>::New(callback);
+	// The baton holds our custom status information for this asynchronous call,
+	// like the callback function we want to call when returning to the main
+	// thread and the status information.
+	Baton* baton = new Baton();
+	baton->error = false;
+	baton->callback = Persistent<Function>::New(callback);
 
-    // This creates the work request struct.
-    uv_work_t *req = new uv_work_t();
-    req->data = baton;
+	// This creates the work request struct.
+	uv_work_t *req = new uv_work_t();
+	req->data = baton;
 
-    // Schedule our work request with libuv. Here you can specify the functions
-    // that should be executed in the threadpool and back in the main thread
-    // after the threadpool function completed.
-    int status = uv_queue_work(uv_default_loop(), req, AsyncWork, AsyncAfter);
-    assert(status == 0);
+	// Schedule our work request with libuv. Here you can specify the functions
+	// that should be executed in the threadpool and back in the main thread
+	// after the threadpool function completed.
+	int status = uv_queue_work(uv_default_loop(), req, AsyncWork, AsyncAfter);
+	assert(status == 0);
 
-    return Undefined();
+	return Undefined();
 }
 //============================================================================
 //  MODULE DECLARATION
@@ -398,26 +407,26 @@ Handle<Value> GetLine(const Arguments& args) {
 void init(Handle<Object> target) {
 	// 2 usable methods :
 	// NODE_SET_METHOD (target, "moduleMethode", c_function);
-	//    or
+	//	or
 	// target->Set(String::NewSymbol("moduleMethod"),
 	//   FunctionTemplate::New(c_function)->GetFunction());
 
 	NODE_SET_METHOD(target, "getLineSync", GetLineSync);
 
-    target->Set(String::NewSymbol("getLine"),
-        FunctionTemplate::New(GetLine)->GetFunction());
+	target->Set(String::NewSymbol("getLine"),
+		FunctionTemplate::New(GetLine)->GetFunction());
 
-    target->Set(String::NewSymbol("getKeySync"),
-        FunctionTemplate::New(GetKeySync)->GetFunction());
+	target->Set(String::NewSymbol("getKeySync"),
+		FunctionTemplate::New(GetKeySync)->GetFunction());
 
-    target->Set(String::NewSymbol("getKey"),
-        FunctionTemplate::New(GetKey)->GetFunction());
+	target->Set(String::NewSymbol("getKey"),
+		FunctionTemplate::New(GetKey)->GetFunction());
 
-    target->Set(String::NewSymbol("setEcho"),
-        FunctionTemplate::New(SetEcho)->GetFunction());
+	target->Set(String::NewSymbol("setEcho"),
+		FunctionTemplate::New(SetEcho)->GetFunction());
 
-    target->Set(String::NewSymbol("setCanonical"),
-        FunctionTemplate::New(SetCanonical)->GetFunction());
+	target->Set(String::NewSymbol("setCanonical"),
+		FunctionTemplate::New(SetCanonical)->GetFunction());
 }
 
 NODE_MODULE(kbd, init);
